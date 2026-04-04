133692bf24b5a015b9fe3c540b08ee6437330f38
Cyt <qinfyy233@gmail.com>
2026-04-04 15:03:25 +0800

    允许忽略合并错误

--- 差分包合并.vb
+++ 差分包合并.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.IO
 Imports System.Text.Json
 Imports System.Text.Json.Nodes
+Imports System.Windows.Forms
 
 Public Class 差分包合并
     Private ReadOnly 日志回调 As Action(Of String)
@@ -123,85 +124,122 @@ Public Class 差分包合并
         If Not IsV2 Then
             Dim hdiffFiles As List(Of String) = File.ReadLines(Path.Combine(差分包目录, "hdifffiles.txt")).ToList()
             For Each line As String In hdiffFiles
-                Dim patchInfo As JsonObject = JsonSerializer.Deserialize(Of JsonObject)(line)
-                Dim remoteName As String = patchInfo("remoteName").ToString()
-                Dim baseName As String = Path.GetFileName(remoteName)
+                Try
+                    Dim patchInfo As JsonObject = JsonSerializer.Deserialize(Of JsonObject)(line)
+                    Dim remoteName As String = patchInfo("remoteName").ToString()
+                    Dim baseName As String = Path.GetFileName(remoteName)
 
-                日志回调($"合并：{baseName}...")
+                    日志回调($"合并：{baseName}...")
 
-                Dim 源文件 As String = Path.Combine(客户端目录, remoteName)
-                Dim hdiff文件 As String = Path.Combine(差分包目录, remoteName & ".hdiff")
-                Dim 目标文件 As String = Path.Combine(临时目录, baseName)
+                    Dim 源文件 As String = Path.Combine(客户端目录, remoteName)
+                    Dim hdiff文件 As String = Path.Combine(差分包目录, remoteName & ".hdiff")
+                    Dim 目标文件 As String = Path.Combine(临时目录, baseName)
 
-                Dim cmd As String = $"""{hpatchzExe}"" ""{源文件}"" ""{hdiff文件}"" ""{目标文件}"""
-                执行命令回调(cmd)
+                    Dim cmd As String = $"""{hpatchzExe}"" ""{源文件}"" ""{hdiff文件}"" ""{目标文件}"""
+                    执行命令回调(cmd)
 
-                If File.Exists(源文件) Then File.Delete(源文件)
-                If File.Exists(hdiff文件) Then File.Delete(hdiff文件)
-                File.Move(目标文件, 源文件)
+                    If File.Exists(源文件) Then File.Delete(源文件)
+                    If File.Exists(hdiff文件) Then File.Delete(hdiff文件)
+                    File.Move(目标文件, 源文件)
+                Catch ex As Exception
+                    日志回调($"合并时出现错误：{ex}")
+
+                    Dim result As DialogResult = MessageBox.Show("合并时出现错误：" & ex.Message & vbCrLf & vbCrLf & "是否忽略并继续？", "警告：", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+                    If result = DialogResult.No Then
+                        Throw ex
+                    End If
+                End Try
             Next
         Else
+            ' V2差分包处理逻辑
             Dim hdiffFiles As HDiffMap = JsonSerializer.Deserialize(Of HDiffMap)(File.ReadAllText(Path.Combine(差分包目录, "hdiffmap.json")))
             For Each json As HDiffData In hdiffFiles.DiffMap
-                Dim sourceFileName As String = json.SourceFileName
-                Dim targetFileName As String = json.TargetFileName
-                Dim patchFileName As String = json.PatchFileName
+                Try
+                    Dim sourceFileName As String = json.SourceFileName
+                    Dim targetFileName As String = json.TargetFileName
+                    Dim patchFileName As String = json.PatchFileName
 
-                日志回调($"合并：{targetFileName}...")
+                    日志回调($"合并：{targetFileName}...")
 
-                Dim 源文件 As String = Path.Combine(客户端目录, sourceFileName)
-                Dim hdiff文件 As String = Path.Combine(差分包目录, patchFileName)
-                Dim 目标文件 As String = Path.Combine(客户端目录, targetFileName)
-                Dim 临时文件 As String = Path.Combine(临时目录, Path.GetFileName(targetFileName))
+                    Dim 源文件 As String = Path.Combine(客户端目录, sourceFileName)
+                    Dim hdiff文件 As String = Path.Combine(差分包目录, patchFileName)
+                    Dim 目标文件 As String = Path.Combine(客户端目录, targetFileName)
+                    Dim 临时文件 As String = Path.Combine(临时目录, Path.GetFileName(targetFileName))
 
-                If Not File.Exists(hdiff文件) Then
-                    日志回调("警告：找不到hdiff文件：" & hdiff文件)
-                    Continue For
-                End If
-
-                Dim cmd As String
-                If Not sourceFileName = "" Then
-                    If Not File.Exists(源文件) Then
-                        日志回调("警告：找不到源文件：" & 源文件)
-                        If File.Exists(hdiff文件) Then File.Delete(hdiff文件)
+                    If Not File.Exists(hdiff文件) Then
+                        日志回调("警告：找不到hdiff文件：" & hdiff文件)
                         Continue For
                     End If
 
-                    cmd = $"""{hpatchzExe}"" ""{源文件}"" ""{hdiff文件}"" ""{临时文件}"""
-                Else
-                    cmd = $"""{hpatchzExe}"" -f """" ""{hdiff文件}"" ""{临时文件}"""
-                End If
-                执行命令回调(cmd)
+                    Dim cmd As String
+                    If Not sourceFileName = "" Then
+                        If Not File.Exists(源文件) Then
+                            日志回调("警告：找不到源文件：" & 源文件)
+                            If File.Exists(hdiff文件) Then File.Delete(hdiff文件)
+                            Continue For
+                        End If
 
-                If File.Exists(源文件) And Not sourceFileName = "" Then File.Delete(源文件)
-                If File.Exists(hdiff文件) Then File.Delete(hdiff文件)
-                If File.Exists(目标文件) Then File.Delete(目标文件)
-                File.Move(临时文件, 目标文件)
+                        cmd = $"""{hpatchzExe}"" ""{源文件}"" ""{hdiff文件}"" ""{临时文件}"""
+                    Else
+                        cmd = $"""{hpatchzExe}"" -f """" ""{hdiff文件}"" ""{临时文件}"""
+                    End If
+                    执行命令回调(cmd)
+
+                    If File.Exists(源文件) And Not sourceFileName = "" Then File.Delete(源文件)
+                    If File.Exists(hdiff文件) Then File.Delete(hdiff文件)
+                    If File.Exists(目标文件) Then File.Delete(目标文件)
+                    File.Move(临时文件, 目标文件)
+                Catch ex As Exception
+                    日志回调($"合并时出现错误：{ex}")
+
+                    Dim result As DialogResult = MessageBox.Show("合并时出现错误：" & ex.Message & vbCrLf & vbCrLf & "是否忽略并继续？", "警告：", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+                    If result = DialogResult.No Then
+                        Throw ex
+                    End If
+                End Try
             Next
         End If
     End Sub
 
     Private Sub 删除文件(文件路径 As String, 文件列表 As List(Of String))
         For Each df As String In 文件列表
-            Dim filePath As String = Path.Combine(文件路径, df.Trim())
-            If File.Exists(filePath) Then
-                File.Delete(filePath)
-                日志回调($"删除：{filePath}")
-            End If
+            Try
+                Dim filePath As String = Path.Combine(文件路径, df.Trim())
+                If File.Exists(filePath) Then
+                    File.Delete(filePath)
+                    日志回调($"删除：{filePath}")
+                End If
+            Catch ex As Exception
+                日志回调($"删除文件时出现错误：{ex}")
+
+                Dim result As DialogResult = MessageBox.Show("删除文件时出现错误：" & ex.Message & vbCrLf & vbCrLf & "是否忽略并继续？", "警告：", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+                If result = DialogResult.No Then
+                    Throw ex
+                End If
+            End Try
         Next
     End Sub
 
     Private Sub 移动文件(源路径 As String, 目标路径 As String)
         For Each 源文件 As String In Directory.GetFiles(源路径, "*", SearchOption.AllDirectories)
-            Dim 相对路径 As String = 源文件.Substring(源路径.Length + 1)
-            Dim 目标文件 As String = Path.Combine(目标路径, 相对路径)
-            Dim 目的文件夹 As String = Path.GetDirectoryName(目标文件)
-            If Not Directory.Exists(目的文件夹) Then
-                Directory.CreateDirectory(目的文件夹)
-            End If
-            日志回调($"移动：{源文件} -> {目标文件}")
-            If File.Exists(目标文件) Then File.Delete(目标文件)
-            File.Move(源文件, 目标文件)
+            Try
+                Dim 相对路径 As String = 源文件.Substring(源路径.Length + 1)
+                Dim 目标文件 As String = Path.Combine(目标路径, 相对路径)
+                Dim 目的文件夹 As String = Path.GetDirectoryName(目标文件)
+                If Not Directory.Exists(目的文件夹) Then
+                    Directory.CreateDirectory(目的文件夹)
+                End If
+                日志回调($"移动：{源文件} -> {目标文件}")
+                If File.Exists(目标文件) Then File.Delete(目标文件)
+                File.Move(源文件, 目标文件)
+            Catch ex As Exception
+                日志回调($"移动文件时出现错误：{ex}")
+
+                Dim result As DialogResult = MessageBox.Show("移动文件时出现错误：" & ex.Message & vbCrLf & vbCrLf & "是否忽略并继续？", "警告：", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+                If result = DialogResult.No Then
+                    Throw ex
+                End If
+            End Try
         Next
     End Sub
 End Class
